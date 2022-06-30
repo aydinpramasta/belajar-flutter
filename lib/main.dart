74a73ffe166114edb0155695496ecbabf989e458
Aydin Pramasta <pramastaaydin@gmail.com>
2022-06-30 20:30:19 +0700

    belajar flutter bagian init project -> bottom navbar

---
+++ lib/main.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String buttonName = 'Lorem';
+  int bottomNavbarIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('First Application'),
+        ),
+        body: Center(
+          child: ElevatedButton(
+            onPressed: () {
+              setState(() {
+                buttonName = (buttonName == 'Lorem') ? 'Ipsum' : 'Lorem';
+              });
+            },
+            child: Text(buttonName),
+          ),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const [
+            BottomNavigationBarItem(
+              label: 'Home',
+              icon: Icon(Icons.home),
+            ),
+            BottomNavigationBarItem(
+              label: 'Settings',
+              icon: Icon(Icons.settings),
+            ),
+          ],
+          currentIndex: bottomNavbarIndex,
+          onTap: (int index) {
+            setState(() {
+              bottomNavbarIndex = index;
+            });
+          },
+        ),
+      ),
+    );
+  }
+}
